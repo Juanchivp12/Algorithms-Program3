6e5b7f9fa02e822f9bd8a54fcced4d78e79259f1
Juanchivp12 <149754308+Juanchivp12@users.noreply.github.com>
2026-04-21 15:19:11 -0500

    Started mergesort to implement my solution

--- prog3-1/Prog3.java
+++ prog3-1/Prog3.java
@@ -1,22 +1,38 @@
 
 public class Prog3 {
 
-
    // you can change the parameters if you want, but you must return the correct answer for that array
-   public int Recur(MyObj[] arr) {   
-          return 0;
+   public int Recur(MyObj[] arr, int left, int right) {
+      if (left >= right) {
+         return 0; // no inversions, one element
+      }
+
+      int mid = (left + right) / 2;
+      int count = 0;
+
+      count += Recur(arr, left, mid);
+      count += Recur(arr,mid + 1, right);
+      count += merge(arr, left, mid + 1, right);
+      return count;
 	}
+
+    private static int merge(MyObj[] arr, int left, int mid, int right) {
+      int count = 0;
+      MyObj[] leftArr = java.util.Arrays.copyOfRange(arr, left, mid);
+      MyObj[] rightArr = java.util.Arrays.copyOfRange(arr, mid, right);
+
+      return count;
+    }
 
 
    public static int Prog3(MyObj[] arr) {
-
       // Preprocessing code here
+      Prog3 prog3 = new Prog3();
 
       // Make a single call to Recur()
-       
-      // Post processing if you want
+      int count = prog3.Recur(arr, 0, arr.length - 1);
 
       // Return the right value
-      return 0;
+      return count;
    }
 }
